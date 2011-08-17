8bf7fca557e4f9e1303118c2fbaa2622238ebf4a
stephenmcd <steve@jupo.org>
2011-08-18 09:49:12 +1000

    Backout

--- app.rb
+++ app.rb
@@ -17,26 +17,26 @@ end
 get "/feed/:key/:name.xml" do
   @user = User.first_or_create(:name => params[:name])
   @user.load_scores params[:key]
-  builder :feed, :locals => {:user => @user}
+  #builder :feed
 
-#builder do |xml|
-#  xml.instruct! :xml, :version => '1.0'
-#  xml.rss :version => "2.0" do
-#    xml.channel do
-#      xml.title "Klout Scores for #{@user}"
-#      xml.description "Klout Scores for #{@user}"
-#      xml.link request.url
-#      @user.scores.latest.each do |score|
-#        xml.item do
-#          xml.title score
-#          xml.link "http://klout.com/##{@user}"
-#          xml.description score
-#          xml.pubDate Time.parse(score.created_at.to_s).rfc822()
-#          xml.guid
-#        end
-#      end
-#    end
-#  end
-#end
+builder do |xml|
+  xml.instruct! :xml, :version => '1.0'
+  xml.rss :version => "2.0" do
+    xml.channel do
+      xml.title "Klout Scores for #{@user}"
+      xml.description "Klout Scores for #{@user}"
+      xml.link request.url
+      @user.scores.latest.each do |score|
+        xml.item do
+          xml.title score
+          xml.link "http://klout.com/##{@user}"
+          xml.description score
+          xml.pubDate Time.parse(score.created_at.to_s).rfc822()
+          xml.guid
+        end
+      end
+    end
+  end
+end
 
 end
--- views/feed.builder
+++ views/feed.builder
@@ -2,13 +2,13 @@ builder do |xml|
   xml.instruct! :xml, :version => '1.0'
   xml.rss :version => "2.0" do
     xml.channel do
-      xml.title "Klout Scores for #{user}"
-      xml.description "Klout Scores for #{user}"
+      xml.title "Klout Scores for #{@user}"
+      xml.description "Klout Scores for #{@user}"
       xml.link request.url
-      user.scores.latest.each do |score|
+      @user.scores.latest.each do |score|
         xml.item do
           xml.title score
-          xml.link "http://klout.com/##{user}"
+          xml.link "http://klout.com/##{@user}"
           xml.description score
           xml.pubDate Time.parse(score.created_at.to_s).rfc822()
           xml.guid
