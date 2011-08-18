0748480eab1770749fca102fe22fea5266e2c573
stephenmcd <steve@jupo.org>
2011-08-18 14:57:20 +1000

    Hook up homepage form

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "builder", "3.0.0"
 gem "datamapper", "1.1.0"
 gem "rest-client", "1.6.3"
 gem "sinatra", "1.2.6"
+gem "erubis", "2.7.0"
 gem "dm-postgres-adapter", :group => :production
 gem "dm-sqlite-adapter", "1.1.0", :group => :development
 gem "sass", "3.1.3"
--- app.rb
+++ app.rb
@@ -1,21 +1,35 @@
 
 require "rubygems"
 require "sinatra"
+require "erubis"
 require "builder"
 require "sass"
 require "./models"
 #require "ruby-debug/debugger"
 
+set :erubis, :escape_html => true
+
+# Show the homepage form.
 get "/" do
-  erb :index
+  erubis :index
 end
 
+# Handle post to the homepage form - display a valid feed URL or error.
+post "/" do
+  @user = User.first_or_create(:name => params[:username])
+  @error = @user.load_scores params[:api_key]
+  @success = @error.length == 0
+  erubis :index
+end
+
+# Render Sass stylesheet.
 get "/stylesheet.css" do
     scss :stylesheet, :style => :compact
 end
 
-get "/feed/:key/:name.xml" do
-  @user = User.first_or_create(:name => params[:name])
+# RSS feed for the given API Key and username.
+get "/feed/:api_key/:username.xml" do
+  @user = User.first_or_create(:name => params[:username])
   @user.load_scores params[:key]
   builder :feed
 end
